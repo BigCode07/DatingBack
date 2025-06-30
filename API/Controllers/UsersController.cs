80420c82f610e29fdcec628830714c06c503b1d4
bigcode <bigcode07@gmail.com>
2025-06-29 21:03:53 -0300

    Feature: Using AutoMapper

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,26 +11,27 @@ using System.Security.Cryptography.X509Certificates;
 namespace API.Controllers;
 
 //[Authorize]
-public class UsersController(IUserRepository userRepository) : BaseApiController
+public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
 {
  
     [HttpGet]
 
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
     {
         // Obtiene todos los usuarios de la base de datos.
         var users = await userRepository.GetUsersAsync();
-        return Ok(users);
+        var usersToReturn = mapper.Map<IEnumerable<MemberDto>>(users);
+        return Ok(usersToReturn);
     }
 
     [HttpGet("{username}")]
     
-    public async Task<ActionResult<AppUser>> GetUser(string username)
+    public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
         // Busca un usuario por su ID.
         var user = await userRepository.GetUserByUsernameAsync(username);
         if (user == null) return NotFound();
-        return user;
+        return mapper.Map<MemberDto>(user);
     }
 
 }
